3fbc08ef11972000d5a821b73c2b5d0ef9ed9339
fedefullone <fedefullone@MacBook-Air-de-Fede.local>
2021-11-15 19:55:22 -0300

    primeros avances index.html

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 window.addEventListener("load",function(){
 
 
-
-fetch('https://api.themoviedb.org/3/movie/76341?api_key=1caaa22005845643c0863fd9677bc21c')
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=1caaa22005845643c0863fd9677bc21c&language=en-US&page=1')
+/*fetch('https://api.themoviedb.org/3/movie/76341?api_key=1caaa22005845643c0863fd9677bc21c')*/
 .then(function(response){
     return response.json();
 
@@ -10,9 +10,19 @@ fetch('https://api.themoviedb.org/3/movie/76341?api_key=1caaa22005845643c0863fd9
 )
 
 .then(function(data){
-    console.log(data);
-}
+    let nombresPeliculasPopulares = data.results[i].title;
+   
+    for  (let i = 0; i < 5; i++) {
+        const cadaPelicula = nombresPeliculasPopulares[i];
+        
+        
+        let peliculaNombre = cadaPelicula.title;
+    }
 
+   
+console.log(nombresPeliculasPopulares);
+
+}
 )
 
 .catch(function(error){
