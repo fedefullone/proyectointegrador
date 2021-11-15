4bb22588799200959c55afb680010dfbe8cb367b
antoniareynal <reynala@udesa.edu.ar>
2021-11-15 15:28:13 -0300

    agregue el window arriba del todo del js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,28 @@
+window.addEventListener("load",function(){
+
+
+
 fetch('https://api.themoviedb.org/3/movie/76341?api_key=1caaa22005845643c0863fd9677bc21c')
 .then(function(response){
     return response.json();
+
 }
 )
 
 .then(function(data){
     console.log(data);
 }
+
 )
 
 .catch(function(error){
     console.log('el error fue:'+ error);
 }
 )
+
+}
+)
+
+
+
 
