c848568e2a7b6886011f96e44397295f845fb7de
antoniareynal <reynala@udesa.edu.ar>
2021-11-15 22:48:13 -0300

    realice el modificaciones en el index.js con respecto al fetch de peliculas populares

--- js/index.js
+++ js/index.js
@@ -1,38 +1,29 @@
+
 window.addEventListener("load",function(){
 
-
 fetch('https://api.themoviedb.org/3/movie/popular?api_key=1caaa22005845643c0863fd9677bc21c&language=en-US&page=1')
-/*fetch('https://api.themoviedb.org/3/movie/76341?api_key=1caaa22005845643c0863fd9677bc21c')*/
+
 .then(function(response){
     return response.json();
+})
 
-}
-)
+
 
 .then(function(data){
-    let nombresPeliculasPopulares = data.results[i].title;
-   
-    for  (let i = 0; i < 5; i++) {
-        const cadaPelicula = nombresPeliculasPopulares[i];
-        
-        
-        let peliculaNombre = cadaPelicula.title;
-    }
 
-   
-console.log(nombresPeliculasPopulares);
+    let peliculasPopulares = document.querySelector(".lista-cartelera")
 
-}
-)
+for (let i = 0; i <5; i++)
+console.log(data.results[i].title)
+})
+
+
 
 .catch(function(error){
-    console.log('el error fue:'+ error);
-}
-)
+    console.log('El error fue: ' + error);
+})
 
 }
 )
 
-
-
-
+        
