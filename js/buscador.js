f7366143e9be599731a2eaac6b6bf195aa958532
Tomas Fontana <tomifontana@hotmail.com>
2021-11-19 00:20:28 -0300

    trabaje en el buscador

--- js/buscador.js
+++ js/buscador.js
@@ -1,40 +1,58 @@
+
 window.addEventListener("load", function(){
 
-    let queryString = location.search
-    let queryStringObj = new URLSearchParams(queryString)
-    let resultadoDeBusqueda = queryStringObj.get("buscador")
+    let queryString = location.search;
+    let queryStringObj = new URLSearchParams(queryString);
+    let resultadoDeBusqueda = queryStringObj.get("busqueda");
     
-    if (resultadoDeBusqueda != null){
-
-        fetch(`${proxy}https://developers.themoviedb.org/3/search/search-movies`)
+    if (resultadoDeBusqueda != null) {
+        fetch(`https://api.themoviedb.org/3/search/movie}`)
             .then(function (response){
                 return response.json();
             })
             .then (function(data){
                 console.log(data);
                 document.querySelector('.resultado').innerHTML = `
-                <h2>${datos.data[0].artist.name}</h2>
+                
                 <div>
-                    <img src="${datos.data[0].artist.picture_big}"> 
-                    <ul>
-                        <li>
-                        <a href="" ><h1>Canciones</h1></a>
-                        </li> 
-                        
-                        <li>
-                        <h1>Album</h1>
-                        </li>
-                    </ul> 
-                </div>`;
+                <img class="portadas" src="https://image.tmdb.org/t/p/w342/${datos.results[i].poster_path}" alt="${datos.results[i].title}">
+                <ul>
+                    <li>
+                    <a ><h1>Movies</h1></a>
+                    </li> 
+                    
+                    <li>
+                    <a href="detail-serie.html?id=${data.results[i].id}">
+                    </li>
+                </ul> 
+            </div>
+                `;
             })
+
+
             .catch(function(error){
                 console.log("el error es:" + error)
             })
 
-            if (resultadoDeBusqueda == "") {
-                this.alert("Debes escribir algo!");
-            }else if (resultadoDeBusqueda.length <= 3)
-                    this.alert("Debes escribir al menos tres caracteres!");
-
+           
     }
+     
+    document.getElementById("form-busqueda").addEventListener("submit", function(e){
+        e.preventDefault();
+        let textoBuscado = document.getElementById("busqueda-text").value;
+        let tieneErrores = false;
+
+        if (textoBuscado == "") {
+            alert("Debes escribir algo!");
+            tieneErrores = true;
+         }else if (textoBuscado.length <= 3){
+            tieneErrores = true;
+            alert("Debes escribir al menos tres caracteres!");
+        }
+        
+        if(!tieneErrores){
+            this.submit();
+        }
+        
+    });
 });
