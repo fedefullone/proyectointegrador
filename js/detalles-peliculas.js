e270d107dce6ab04ed3154c8819ce602b01d9bdf
antoniareynal <reynala@udesa.edu.ar>
2021-11-20 17:21:38 -0300

    hice la pagina de favoritos de series

--- js/detalles-peliculas.js
+++ js/detalles-peliculas.js
@@ -10,7 +10,10 @@ fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=1caaa22005845643c08
 
     .then(function (data) {
         console.log(data);
+
+        let arrayFav = [data];
         let infoPeliculas = document.querySelector('section');
+        console.log(data.genres[1])
         infoPeliculas.innerHTML =
             ` <h2 class="h2-titulos">${data.title}</h2>
     
@@ -24,16 +27,30 @@ fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=1caaa22005845643c08
               <li>Release date: ${data.release_date}
               <li>Duration: ${data.runtime} minutes</li>
               <li>
-                   <a class="a-li-detail-serie"href="detail-accion.html">Género: ${data.genres}</a>
+                   <a class="a-li-detail-serie"href="detail-accion.html">Género: ${data.genres[1].name}</a>
                </li>
               <li>Synopsis: ${data.overview}</li>
             
                </ul>
-               <h3><a class="boton-favoritos" href="favoritos.html?=${data.id}">Agregar a favoritos ♥ ♥ ♥</a></h3>
+               <h3><a class="boton-favoritos">Agregar a favoritos ♥ ♥ ♥</a></h3>
              
        </article>
    
 </div>`
+
+        let fav = document.querySelector('.boton-favoritos')
+        fav.addEventListener('click', function(){
+            if(window.localStorage.getItem('favoritos')== null){
+            window.localStorage.setItem('favoritos', JSON.stringify(arrayFav))
+            console.log(arrayFav)
+            } else{
+                let peliculaObjeto = JSON.parse(window.localStorage.getItem('favoritos'))
+                peliculaObjeto.push(data)
+                window.localStorage.setItem('favoritos', JSON.stringify(peliculaObjeto))
+                console.log(peliculaObjeto);
+            }
+        })
+       
     })
 
     .catch(function (error) {
--- js/favoritos.js
+++ js/favoritos.js
@@ -1,24 +1,31 @@
 // favoritos peliculas
 window.addEventListener("load", function () {
+    let listaPelis = window.localStorage.getItem('favoritos') 
+    let pelis = JSON.parse(listaPelis)
+    let contenedorFavoritos = document.querySelector('.section-favoritos')
+    for (let i = 0; i < pelis.length; i++) {
+        const element = pelis[i];
+        contenedorFavoritos.innerHTML += `
+        <article class="listados">
+    <div>
+    <a href="detail-movie.html?id=${element.id}">
+        <img class="portadas"src="https://image.tmdb.org/t/p/w342/${element.poster_path}"alt="${element.title}">
+  </a>
+        </div>
 
-    let queryStringObj = new URLSearchParams(location.search);
-    let id = queryStringObj.get('id');
+<h3 class="titulos-peliculas"><a href="detail-movie.html?id=${element.id}" class="a-titulo">${element.title}</a></h3>
+<p class="generos-texto"><a href="detail-movie.html?id=${element.id}" class="a-titulo">${element.release_date}</a></p>
+<p class="generos-texto"><a href="detail-movie.html?id=${element.id}" class="a-titulo"> See more ✓ </a></p>
 
-    // selecciono el boton de favoritos
-    let botonFavoritos = document.querySelector('.boton-favoritos')
 
-    // defino el array para guardas las peliculas favoritas
-    let favoritos = [];
-    let traigoStorage = localStorage.getItem('favoritos');
-    if(traigoStorage){
-        favoritos = JSON.parse(traigoStorage);
-
-    }
+</article> `
     
+    }
 
-
-
-
+let vaciar = document.querySelector('.vaciar-favoritos')
+vaciar.addEventListener('click', function(){
+    window.localStorage.clear('favoritos')
+})
 
 
 })
